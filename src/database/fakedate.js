da3b4f88c8a7aeffe62576fc53d5ef8671df0c37
Diego <diegoferreiradoamaral@gmail.com>
2020-11-07 17:43:09 -0300

    back end com Handlebars

---
+++ src/database/fakedate.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    id: 1,
+    lat: "-23.687977",
+    lng: "-46.6802013",
+    name: "Lar das meninas",
+    description:
+      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1586978450997-3251b1d493e9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+      "https://images.unsplash.com/photo-1572248364230-7f412885f2da?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ],
+    instructions:
+      "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+    opening_hours: "Horários de visitas Das 18h até 8h",
+    open_on_weekends: "1",
+  },
+  {
+    id: 2,
+    lat: "-23.6935669",
+    lng: "-46.6829917",
+    name: "Lar dos meninos",
+    description:
+      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1586978450997-3251b1d493e9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+      "https://images.unsplash.com/photo-1572248364230-7f412885f2da?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ],
+    instructions:
+      "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+    opening_hours: "Horários de visitas Das 18h até 8h",
+    open_on_weekends: "0",
+  },
+];
